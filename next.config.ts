412430a2f07e49ce09a65e4ed7bacb904943f675
Manolo Cerezuela <manolocerezuela4@gmail.com>
2025-09-25 14:55:29 +0000

    Runtime Error NotAllowedError: Failed to execute 'writeText' on 'Clipboa

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'busqydyuvtgwemjxmylp.supabase.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   webpack(config) {
