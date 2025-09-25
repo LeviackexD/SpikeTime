5330cd1d0bcf3af149c914027c6a1fcb415598e7
Manolo Cerezuela <manolocerezuela4@gmail.com>
2025-09-25 20:10:48 +0000

    como cambio el icono (_for element <VolleyballIcon>_)

--- src/components/icons/volleyball-icon.tsx
+++ src/components/icons/volleyball-icon.tsx
@@ -1,3 +1,4 @@
+
 import * as React from 'react';
 
 export function VolleyballIcon(props: React.SVGProps<SVGSVGElement>) {
@@ -14,10 +15,12 @@ export function VolleyballIcon(props: React.SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <circle cx="12" cy="12" r="10" />
-      <path d="M12 12a10 10 0 0 0-8.66-5" />
-      <path d="M12 12a10 10 0 0 1 8.66-5" />
-      <path d="M12 12a10 10 0 0 0 0 10" />
+        <path d="M22 17a5 5 0 0 1-5.6-4H8.4A5 5 0 0 1 3 17" />
+        <path d="M4 17c0-2.3 1-4.3 2.5-5.5" />
+        <path d="M17.5 11.5c1.5 1.2 2.5 3.2 2.5 5.5" />
+        <path d="M12 8a2 2 0 0 0-2 2c0 1.1.9 2 2 2s2-.9 2-2c0-1.1-.9-2-2-2z" />
+        <path d="M8.4 13c-2 .5-3.4 2.2-3.4 4" />
+        <path d="M19 17c0-1.8-1.4-3.5-3.4-4" />
     </svg>
   );
 }
